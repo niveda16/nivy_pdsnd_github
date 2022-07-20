78604a9225d5c2d9d01f290ac3459c91857b5246
Niveda Iyer <nivy1624@gmail.com>
2022-07-20 08:24:55 -0700

    modified docstrings for get_filters function

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -15,8 +15,8 @@ def get_filters():
 
     Returns:
         (str) city - name of the city to analyze
-        (str) month - name of the month to filter by, or "none" to apply no month filter
-        (str) day - name of the day of week to filter by, or "none" to apply no day filter
+        (str) month - name of the month to filter by, or "all" to apply no month filter
+        (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
     print('Hello! Let\'s explore some US bikeshare data for Chicago, New York City, Washington!')
     # get user input for city (chicago, new york city, washington).
